1846e647d10362eebee564b74f32071c28535afc
Danill Yunusov <dr-0901@mail.ru>
2023-11-20 15:27:35 +0300

    work with Hibernate

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,10 +1,22 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
+
+    private final SessionFactory sessionFactory = Util.getSessionFactory();
+    private Transaction transaction;
+    private final String CREATE_TABLE = "CREATE TABLE IF NOT EXISTS users (id BIGINT PRIMARY KEY NOT NULL AUTO_INCREMENT, " +
+            "name VARCHAR(20) NOT NULL, " +
+            "lastname VARCHAR(20) NOT NULL, " +
+            "age TINYINT UNSIGNED NOT NULL)";
+    private final String DROP_TABLE = "DROP TABLE IF EXISTS users";
     public UserDaoHibernateImpl() {
 
     }
@@ -12,31 +24,84 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void createUsersTable() {
-
+        try(Session session = sessionFactory.getCurrentSession()) {
+            session.beginTransaction();
+            session.createSQLQuery(CREATE_TABLE).addEntity(User.class).executeUpdate();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            System.out.println("TABLE WAS NOT CREATED");
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void dropUsersTable() {
-
+        try(Session session = sessionFactory.getCurrentSession()) {
+            session.beginTransaction();
+            session.createSQLQuery(DROP_TABLE).addEntity(User.class).executeUpdate();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            System.out.println("TABLE WAS NOT DROP");
+        }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-
+        try(Session session = sessionFactory.getCurrentSession()) {
+            transaction = session.beginTransaction();
+            User user = new User(name, lastName, age);
+            session.save(user);
+            transaction.commit();
+        } catch (Exception e) {
+            System.out.println("USER WAS NOT SAVED");
+            e.printStackTrace();
+            if(transaction != null) {
+                transaction.rollback();
+            }
+        }
     }
 
     @Override
     public void removeUserById(long id) {
-
+        try(Session session = sessionFactory.getCurrentSession()) {
+            transaction = session.beginTransaction();
+            session.delete(session.get(User.class, id));
+            transaction.commit();
+        } catch (Exception e) {
+            System.out.println("USER WAS NOT REMOVE");
+            if(transaction != null) {
+                transaction.rollback();
+            }
+        }
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        List<User> userList = null;
+        try(Session session = sessionFactory.getCurrentSession()) {
+            transaction = session.beginTransaction();
+            userList = session.createQuery("from User").getResultList();
+            transaction.commit();
+        } catch (Exception e) {
+            System.out.println("USERS NOT RECEIVED");
+            if(transaction != null) {
+                transaction.rollback();
+            }
+        }
+        return userList;
     }
 
     @Override
     public void cleanUsersTable() {
-
+        try(Session session = sessionFactory.getCurrentSession()) {
+            transaction = session.beginTransaction();
+            session.createQuery("delete User").executeUpdate();
+            transaction.commit();
+        } catch (Exception e) {
+            System.out.println("USER TABLE HAS NOT BEEN CLEARED");
+            if(transaction != null) {
+                transaction.rollback();
+            }
+        }
     }
 }
